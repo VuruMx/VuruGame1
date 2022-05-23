fbedc9e938f25a9ad5ba10dea7038956acdaf0ad
VuruMx <105889011+VuruMx@users.noreply.github.com>
2022-05-23 05:03:30 -0500

    Add files via upload

---
+++ Red/config.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+    /**
+     * Provide your MYSQL database credentials
+     */
+    'db_host' => 'localhost',
+    'db_username' => 'root',
+    'db_name' => 'tinsta',
+    'db_password' => '',
+
+    /**
+     * Option to know if debug is enabled or not
+     */
+    'debug' => true,
+
+    /**
+     * Option to enable https
+     */
+    'https' => false,
+
+    /**
+     * cookie path
+     */
+    'cookie_path' => '/',
+
+    'api-key' => 'normalKey',
+
+    'rtl-langs' => array(
+        'ar',
+        'fa',
+    ),
+
+    'installed' => false
+);
